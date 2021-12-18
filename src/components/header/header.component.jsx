3a0493a1c31d3aebdd26a9d05ca116ac7a9e670a
Nikesh Sitaula <31296289+nikeshsitaula@users.noreply.github.com>
2021-12-19 09:45:02 +1100

    cart icon added and cart drop down components to our applicati

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,7 +8,10 @@ import {ReactComponent as Logo} from '../../assets/logo.svg'; //syntax in react 
 
 import {auth} from '../../firebase/firebase.utils';
 
-const Header = ({currentUser}) => (
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo'/>
@@ -30,12 +33,17 @@ const Header = ({currentUser}) => (
                     SIGN IN
                 </Link>
             )}
+            <CartIcon/>
         </div>
+        {hidden? null:
+        <CartDropdown/>
+        }
     </div>
 );
 
-const mapStateToProps = state => ({
- currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
